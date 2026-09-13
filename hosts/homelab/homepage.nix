4935a0bae30dc6e8535844ebb63b356d165ed883
austin <austin@austingw.com>
2026-09-13 00:27:45 -0400

    Fix missing allowedHosts

--- hosts/homelab/homepage.nix
+++ hosts/homelab/homepage.nix
@@ -2,6 +2,7 @@
   services.homepage-dashboard = {
     enable = true;
     listenPort = 3333;
+    allowedHosts = "homepage.stoat-wyvern.ts.net,localhost:3333,127.0.0.1:3333";
 
     settings = {
       title = "Austin's Homelab";
